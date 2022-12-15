c3a5208c9d67df687a27789318bdc06e84e038ea
LeonardoCerasi <cerasi.leonardo03@gmail.com>
2022-12-15 01:13:59 +0100

    Added files for Esercitazione 7

---
+++ lab7/circle.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+
+#include "circle.h"
+#include "func.h"
+
+int main()
+{
+    std::ofstream output;
+    output.open("output.txt");
+
+    int dim_circles = lines("cerchi.in");
+    int dim_codices = lines("codici.in");
+
+    circle* circles = new circle[dim_circles];
+    int* codices = new int[dim_codices];
+
+    set_circles(circles, dim_circles);
+    set_codices(codices, dim_codices);
+
+    std::cout << "\nCodices:" << std::endl;
+
+    for(int i = 0; i < dim_codices; i++)
+    {
+        std::cout << codices[i] << std::endl;
+    }
+    std::cout << std::endl;
+
+    int index = select(circles, dim_circles, codices, dim_codices);
+
+    std::cout << (dim_circles - index) << " circles were eliminated and " << (index + 1) << " circles remain.\n" << std::endl;
+
+    for (int i = 0; i < dim_codices; i++)
+    {
+        int number = 0;
+        float x,y = 0;
+
+        std::cout << codices[i] << ": ";
+
+        for (int j = 0; j <= index; j++)
+        {
+            if (circles[j].c == codices[i])
+            {
+                number++;
+                x += circles[j].x;
+                y += circles[j].y;
+            }
+        }
+
+        float bar_x = x / number;
+        float bar_y = y / number;
+
+        std::cout << number << std::endl;
+        std::cout << "B(" << bar_x << "," << bar_y << ")\n" << std::endl;
+
+        output << codices[i] << ": " << number << std::endl << std::endl;
+
+        float d;
+
+        for(int j = 0; j <= index; j++)
+        {
+            if (circles[j].c == codices[i])
+            {
+                d = dist(circles[j].x, circles[j].y, bar_x, bar_y);
+
+                print_to_file(output, circles[j], d);
+            }
+        }
+
+        output << std::endl;
+    }
+
+    output.close();
+
+    return 0;
+}
---
+++ lab7/circle.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+struct circle
+{
+    float x,y,r,c;
+};
+
+// Set methods
+
+void set_circles(circle* circles, int dim)
+{
+    std::ifstream input;
+    input.open("cerchi.in");
+
+    for (int i = 0; i < dim; i++)
+    {
+        input >> circles[i].x;
+        input >> circles[i].y;
+        input >> circles[i].r;
+        input >> circles[i].c;
+    }
+
+    input.close();
+}
+void set_codices(int* codices, int dim)
+{
+    std::ifstream input;
+    input.open("codici.in");
+
+    for (int i = 0; i < dim; i++)
+    {
+        input >> codices[i];
+    }
+
+    input.close();
+}
+
+// Print methods
+
+void print_circle(circle C)
+{
+    std::cout << "C(" << C.x << "," << C.y << ")\t" << C.r << "\t" << C.c << std::endl;
+}
+
+void print_to_file(std::ofstream& output, circle C, float d)
+{
+    output << "C(" << C.x << "," << C.y << ")\t" << C.r << "\t" << C.c << "\t" << d << std::endl;
+}
+
+// Other methods
+
+bool exists_code(circle C, int* codices, int dim)
+{
+    bool flag = false;
+
+    for (int i = 0; i < dim; i++)
+    {
+        if (codices[i] == C.c)
+        {
+            flag = true;
+        }
+    }
+
+    return flag;
+}
+
+float dist(float x1, float y1, float x2, float y2)
+{
+    return sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+}
